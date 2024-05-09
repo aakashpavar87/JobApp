b8449aa62218c5fe80bd590b11e3f7a28980d4df
Aakash Pavar <aakashpavar87@gmail.com>
2024-05-09 19:00:19 +0530

    made little chages to urls.py

--- subscribe/urls.py
+++ subscribe/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('subscribe/', views.subscribe, name="subscribe")
+    path('subscribe', views.subscribe, name="subscribe")
 ]
