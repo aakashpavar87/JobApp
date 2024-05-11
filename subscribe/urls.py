5d71b6af5029d47319d968535845eefd293441f1
Aakash Pavar <aakashpavar87@gmail.com>
2024-05-11 13:56:31 +0530

    nothing changed

--- subscribe/urls.py
+++ subscribe/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('subscribe', views.subscribe, name='subscribe')
+    path('subscribe', views.subscribe, name='subscribe'),
+    path('thank-you', views.thank_you, name='thanks')
 ]
--- subscribe/views.py
+++ subscribe/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.urls import reverse
 
 from subscribe.forms import SubscribeForm
 from subscribe.models import Subscribe
+
+def thank_you(request):
+    return render(request, 'subscribe/thankYou.html', context={})
 
 # Create your views here.
 def subscribe(request):
@@ -12,9 +16,19 @@ def subscribe(request):
         first_name = request.POST['first_name']
         last_name = request.POST['last_name']
         subscribe_form = SubscribeForm(request.POST)
-        if subscribe_form.is_valid:
-            print(subscribe_form.cleaned_data)
-        # Subscribe.objects.create(first_name=first_name, last_name=last_name, email=email)
-    
+
+        if subscribe_form.is_valid():
+            print('Valid Form')
+            cleaned_data = subscribe_form.cleaned_data
+            print(cleaned_data)
+
+            # Now you can access cleaned data for saving to the database
+            Subscribe.objects.create(
+                first_name=cleaned_data['first_name'],
+                last_name=cleaned_data['last_name'],
+                email=cleaned_data['email']
+            )
+            return redirect(reverse('thanks'))
+
     context = {'form': subscribe_form}
     return render(request, 'subscribe/subscribe.html', context)
