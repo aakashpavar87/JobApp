0765c3aa70ca23d0625da666063f522aca7d4c59
Aakash Pavar <aakashpavar87@gmail.com>
2024-05-15 00:23:01 +0530

    added postgreSQL configuration to deployment

--- JobApp/settings.py
+++ JobApp/settings.py
@@ -86,10 +86,21 @@ WSGI_APPLICATION = 'JobApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USERNAME'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOSTNAME'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
